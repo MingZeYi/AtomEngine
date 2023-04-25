2d970165320557ca657f98867664a616f53fa32f
unknown <18942807615@163.com>
2023-04-25 14:37:54 +0800

    init

--- AtomEngine/src/AtomEngine.h
+++ AtomEngine/src/AtomEngine.h
@@ -2,9 +2,9 @@
 
 //For use by AtomEngine application
 
-#include <stdio.h>
 #include "AtomEngine/Application.h"
 
 //Entry Point-----------------------
 #include "AtomEngine/EntryPoint.h"
 // ---------------------------------
+
